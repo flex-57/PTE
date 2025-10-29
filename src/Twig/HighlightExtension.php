b9945211de1d16f5b1d48300964a0066d0d49694
flex-57 <flex57@gmx.fr>
2025-10-29 15:32:55 +0100

    ajout svg logo + highlight sur le terme du search ...

---
+++ src/Twig/HighlightExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class HighlightExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('highlight_query', [$this, 'highlightQuery'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function highlightQuery(string $text, string $query): string
+    {
+        if (empty($query)) {
+            return $text;
+        }
+
+        $escapedQuery = preg_quote($query, '/');
+
+        return preg_replace(
+            "/($escapedQuery)/i", // insensible à la casse
+            '<span class="bg-yellow-200 font-semibold text-black rounded-sm">$1</span>',
+            $text
+        );
+    }
+}
